511a143f6ca6fef1d8aa594973a9070c15c4e928
vinitathopte2603 <vinitathopte1@outlook.com>
2020-05-04 00:57:40 +0530

    [Vinita]ADDED:DetermineToCheckWinnerOrChangeTurn

--- ticTacToe.sh
+++ ticTacToe.sh
@@ -4,16 +4,26 @@ var="Welcome to tic tac toe game"
 echo "$var"
 
 declare -a board
-PLAYER=X;
-COMPUTER=O;
+
+GAMESTATUS=1;
+NUMBEROFROWS=3;
+NUMBEROFCOLUMNS=3;
 HEAD=1;
 TAIL=0;
 BOARD_POSITION=10;
+winner=false;
+turn=0
+player=''
+computer=''
+
+
+function boardInitialisation(){
 
 for (( i=1;i<=$BOARD_POSITION;i++ ))
 do
 	board[$i]=0
 done
+}
 
 function printBoard(){
 
@@ -26,28 +36,116 @@ function printBoard(){
 	echo " -------------"
 }
 
+function checkHorizontal(){
+	numberOfRows=3
+	position=1
+	counter=1
+	while [ $counter -le 3 ]
+	do
+		if [[ ${board[$position]} == ${board[$position+1]} ]] && [[ ${board[$position+1]} == ${board[$position+2]} ]] && [[ ${board[$position+2]} == $1 ]]
+	then
+		printBoard
+		echo "$1 wins"
+		winner=true
+		break
+	else
+		position=$(( $position+$numberOfRows ))
+	fi
+	counter=$(( $counter+1 ))
+	done
+}
+
+function checkVertical(){
+	numberOfRows=3
+	position=1
+	counter=1
+	while [ $counter -le 3 ]
+	do
+		if [[ ${board[$position]} == ${board[$position+3]} ]] && [[ ${board[$position+3]} == ${board[$position+6]} ]] && [[ ${board[$position+6]} == $1 ]]
+		then
+			printBoard
+			echo "$1 wins"
+			winner=true
+			break
+		else
+		position=$(( $position+1 ))
+		fi
+	counter=$(( $counter+1 ))
+	done
+}
+
+function checkDiagonal(){
+
+	position=1
+	counter=1
+	while [ $counter -le 2 ]
+	do
+		if [[ ${board[$position]} == ${board[$positon+4]} ]] && [[ ${board[$position+4]} == ${board[$position+8]} ]] && [[ ${board[$position+8]} == $1 ]]
+		then
+		printBoard
+		echo "$1 wins"
+		winner=true
+		break
+		elif [[ ${board[$position+2]} == ${board[$position+4]} ]] && [[ ${board[$position+4]} == ${board[$position+6]} ]] && [[ ${board[$position+6]} == $1 ]]
+		then
+		printBoard
+		echo "$1 wins"
+		winner=true
+		break
+	fi
+	counter=$(($counter+1))
+	done
+
+}
+
 function userInput(){
+	count=0
+	if [ $(($turn%2)) -eq 0 ]
+	then
 
 	read -p "Enter your choice : " POSITION
-	if [ $POSITION -gt 10 ]
+	if [ ${board[$POSITION]} -eq 0 ]
 	then
-		echo "Invalid Input"
+		board[$POSITION]=$player
+		printBoard
+		turn=$(( $turn + 1 ))
+	else
+		echo "Enter number between 1 to 10"
+		userInput
 	fi
-	board[$POSITION]=$PLAYER
+	else
+	echo "Computer's turn"
 	printBoard
+	turn=$(( $turn + 1 ))
+	fi
 
 }
 
 function ticTacToe(){
-checkToss=$((RANDOM%2))
-if [[ $checkToss == $HEAD ]]
+	checkToss=$((RANDOM%2))
+	if [[ $checkToss -eq $HEAD ]]
 then
+	player='X'
+	computer='O'
 	echo "Player plays first"
 	printBoard
 	userInput
+
 else
-	echo "Computer plays First"
-	printBoard
-fi
+	player='O'
+	computer='X'
+	echo "Computer plays first"
+	fi
 }
+
+boardInitialisation
 ticTacToe
+
+while [ $winner == false ]
+do
+	printBoard
+	userInput
+	checkHorizontal $player
+	checkVertical $player
+	checkDiagonal $player
+done
