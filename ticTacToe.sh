c8a35b1139531b7aaf77cfcdb023f6765c262eea
vinitathopte2603 <vinitathopte1@outlook.com>
2020-05-03 22:57:40 +0530

    [Vinita]ADDED:DisplayBoardToChooseValidPosition

--- ticTacToe.sh
+++ ticTacToe.sh
@@ -1,4 +1,4 @@
-#!/bin/bash -x
+#!/bin/bash
 
 var="Welcome to tic tac toe game"
 echo "$var"
@@ -8,13 +8,46 @@ PLAYER=X;
 COMPUTER=O;
 HEAD=1;
 TAIL=0;
+BOARD_POSITION=10;
 
-board=(0,0,0,0,0,0,0,0,0)
+for (( i=1;i<=$BOARD_POSITION;i++ ))
+do
+	board[$i]=0
+done
 
+function printBoard(){
+
+	echo " -------------"
+	echo " | "${board[1]}" | "${board[2]}" | "${board[3]}" | "
+	echo " -------------"
+	echo " | "${board[4]}" | "${board[5]}" | "${board[6]}" | "
+	echo " -------------"
+	echo " | "${board[7]}" | "${board[8]}" | "${board[9]}" | "
+	echo " -------------"
+}
+
+function userInput(){
+
+	read -p "Enter your choice : " POSITION
+	if [ $POSITION -gt 10 ]
+	then
+		echo "Invalid Input"
+	fi
+	board[$POSITION]=$PLAYER
+	printBoard
+
+}
+
+function ticTacToe(){
 checkToss=$((RANDOM%2))
 if [[ $checkToss == $HEAD ]]
 then
 	echo "Player plays first"
+	printBoard
+	userInput
 else
 	echo "Computer plays First"
+	printBoard
 fi
+}
+ticTacToe
