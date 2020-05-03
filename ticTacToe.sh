2baee09e8dada6ce171523d4d138a43c2f690b4e
vinitathopte2603 <vinitathopte1@outlook.com>
2020-05-03 22:15:20 +0530

    [Vinita]ADDED:InitialisingTheTicTacToeBoard

--- ticTacToe.sh
+++ ticTacToe.sh
@@ -2,3 +2,13 @@
 
 var="Welcome to tic tac toe game"
 echo "$var"
+
+declare -a board
+NUMBER_OF_ROWS=3
+NUMBER_OF_COLUMNS=3
+
+function InitializingBoard(){
+board=(0,0,0,0,0,0,0,0,0)
+}
+
+InitializingBoard
